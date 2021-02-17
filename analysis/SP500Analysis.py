0a92a333a854cd2a96b40bcb25de0120473aafe0
sianwatkins# <sianewatkins101@outlook.com>
2021-02-17 19:17:45 +0000

    New Python File Changes

--- analysis/SP500Analysis.py
+++ analysis/SP500Analysis.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import plotly.express as px
 SP500 = pd.read_csv('C:\\Users\\siane\\OneDrive\\Documents\\FinMar_Python\\data_set\\SP500_data.csv')
 
 print(SP500)
+
+fig = px.bar(SP500, x="Date", y="SP500")
+fig.show()
