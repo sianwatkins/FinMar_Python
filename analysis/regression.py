4af00c95fdacec8f3a6d4683cad2fbd25897642b
sianwatkins# <sianewatkins101@outlook.com>
2021-04-07 16:18:13 +0100

    regression analysis

--- analysis/regression.py
+++ analysis/regression.py
@@ -5,17 +5,34 @@ import statsmodels.formula.api as sm
 SP5002 = pd.read_csv('C:\\Users\\siane\\OneDrive\\Documents\\FinMar_Python\\data_set\\SP500_data.csv')
 
 
-def lobf():
+def lobf(SP5002):
     # effect of dividend on the SP500 index
     graph2 = px.scatter(SP5002, x='Dividend', y='SP500', trendline='ols')
     graph2.show()
     return graph2
 
 
-scattergraph = lobf()
-A = SP5002["SP500"]
-B = SP5002["Dividend"]
-C = SP5002["Long Interest Rate"]
-result = sm.ols(formula="A ~ B + C", data=SP5002).fit()
-print(result.params)
-print(result.summary())
+def regress(SP5002):
+    Y = SP5002["SP500"]
+    BetaHAT1 = SP5002["Dividend"]
+    BetaHAT2 = SP5002["Long Interest Rate"]
+    d = {"Y": pd.Series(Y), "BetaHAT1": pd.Series(BetaHAT1), "BetaHAT2": pd.Series(BetaHAT2)}
+    df = pd.DataFrame(d)
+    result = sm.ols(formula="Y ~ BetaHAT1 + BetaHAT2", data=df).fit()
+    sian = result.summary()
+    print(sian)
+    return sian
+
+
+# def interpret(sian):
+# print("Hypothesis Test for the Significance of B")
+# print(sian)
+
+
+lobf(SP5002)
+reg = regress(SP5002)
+# interpret(reg)
+
+##############Check if something is a dataframe (HELPFUL)
+# if isinstance(sian, pd.DataFrame):
+#   print("YESSSSS")
